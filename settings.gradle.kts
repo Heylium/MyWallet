c326625b5c98461d417841732437fdf6d66245d7
Heylium <tuanhsiang@163.com>
2023-12-07 00:50:48 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,35 @@
+pluginManagement {
+    repositories {
+
+        // TODO: set maven mirror repo, comment if necessary
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+
+        // TODO: set maven mirror repo, comment if necessary
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/jcenter") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
+        maven { setUrl("https://jitpack.io") }
+
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MyWallet"
+include(":app")
+
