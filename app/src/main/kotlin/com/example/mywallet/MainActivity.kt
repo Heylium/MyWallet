a6d146736b03c4686307b20d7a62f095598f2681
Heylium <tuanhsiang@163.com>
2023-12-09 01:24:08 +0800

    New app added to MainActivity.

--- app/src/main/kotlin/com/example/mywallet/MainActivity.kt
+++ app/src/main/kotlin/com/example/mywallet/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.mywallet.navigation.*
 import com.example.mywallet.ui.theme.MyWalletTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +27,7 @@ class MainActivity : ComponentActivity() {
             MyWalletTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android My Wallet")
+                    MyApp()
                 }
             }
         }
@@ -27,17 +35,32 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyApp() {
+    MyWalletTheme{
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyWalletTheme {
-        Greeting("Android")
+        val navController = rememberNavController()
+        val bottomBarState = rememberSaveable { mutableStateOf(true) }
+        val currentBackState by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackState?.destination
+        val bottomBarCurrentScreen = bottomBarItems.find { it.route == currentDestination?.route } ?: Home
+
+
+        Surface {
+            Scaffold(
+                bottomBar = {
+                    AppBottomBarM3(
+                        bottomBarState = bottomBarState,
+                        bottomBarItems = bottomBarItems,
+                        bottomBarCurrentScreen = bottomBarCurrentScreen,
+                        onClick = { itemScreen -> navController.restoreStateOnReturn(itemScreen.route) }
+                    )
+                }
+            ) {
+                AppHost(
+                    navController = navController,
+                    Modifier.padding(it)
+                )
+            }
+        }
     }
 }
